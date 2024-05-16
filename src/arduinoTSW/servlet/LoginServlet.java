7c23978735c37f1ee41340df7f8b2772865fc23e
lprisco05 <lucaprisco636@gmail.com>
2024-05-17 00:49:38 +0200

    Implementazione messaggi di errore nel login

--- src/arduinoTSW/servlet/LoginServlet.java
+++ src/arduinoTSW/servlet/LoginServlet.java
@@ -2,12 +2,17 @@ package arduinoTSW.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import arduinoTSW.connection.DbCon;
+import arduinoTSW.dao.UserDao;
+import arduinoTSW.model.User;
 
 @WebServlet("/user-login")
 public class LoginServlet extends HttpServlet {
@@ -24,7 +29,29 @@ public class LoginServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		try(PrintWriter out = response.getWriter())
 		{
-			out.print("LOGIN WHIT SERVLET");
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+			
+			try {
+				UserDao udao = new UserDao(DbCon.getConnection());
+				User user = udao.userLogin(email,  password);
+				
+				if(user != null)
+				{
+					out.print("user login");
+				}
+				else
+				{
+					out.print("user login failed");
+				}
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				
+				e.printStackTrace();
+			}
+			
+			
 		}
 	}
 
