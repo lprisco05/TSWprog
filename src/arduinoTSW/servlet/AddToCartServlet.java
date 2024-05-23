bea1c755c15a3033fbc19e91c9635a235fc90825
lprisco05 <lucaprisco636@gmail.com>
2024-05-23 15:56:39 +0200

    Creazione sessione per aggiungi a carrello

---
+++ src/arduinoTSW/servlet/AddToCartServlet.java
@@ -0,0 +1,53 @@
+package arduinoTSW.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import arduinoTSW.model.*;
+
+/**
+ * Servlet implementation class AddToCartServlet
+ */
+@WebServlet("/add-to-cart")
+public class AddToCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		try(PrintWriter out = response.getWriter()){
+			ArrayList<Cart> cartList = new ArrayList<>();
+			
+			int id = Integer.parseInt(request.getParameter("id"));
+			Cart cm = new Cart();
+			cm.setId(id);
+			cm.setQuantity(1);
+			
+			
+			HttpSession session = request.getSession();
+			ArrayList<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list");
+			
+			if(cart_list == null)
+			{
+				cartList.add(cm); 
+				session.setAttribute("cart-list", cart_list);
+				out.println("session created and added the list");
+			}
+			
+		}
+		
+	}
+
+}
