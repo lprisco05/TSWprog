e7e3523102c38b5af52afbf42557f286cd3294e9
pacifi5 <andrea.p.villano@hotmail.com>
2024-05-25 11:57:01 +0200

    Decrement to 0 fa la remove

--- src/arduinoTSW/servlet/QuantityIncDecServlet.java
+++ src/arduinoTSW/servlet/QuantityIncDecServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,35 +20,39 @@ public class QuantityIncDecServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
+		
 		try (PrintWriter out = response.getWriter()) {
+			int quantity = -1;
 			String action = request.getParameter("action");
 			int id = Integer.parseInt(request.getParameter("id"));
-			boolean found = false;
+			System.out.println("huh?" + id);
 			ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
 
 			if (action != null && id >= 1) {
 				if (action.equals("inc")) {
 					for (Cart c : cart_list) {
 						if (c.getId() == id) {
-							found=true;
 							c.setQuantity(c.getQuantity()+1);
 							response.sendRedirect("cart.jsp");
 						}
 					}
 				}
-
 				if (action.equals("dec")) {
 					for (Cart c : cart_list) {
-						if (c.getId() == id && c.getQuantity() > 1) {
-							found=true;
+						if (c.getId() == id && c.getQuantity() > 0) {
 							c.setQuantity(c.getQuantity()-1);
+							quantity=c.getQuantity();
+							break;
 						}
+					}
+					if(quantity == 0) {
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/remove-from-cart");
+						dispatcher.forward(request, response);
+						return;
 					}
 					response.sendRedirect("cart.jsp");
 				}
-				if(found==false) {
-					response.sendRedirect("cart.jsp");
-				}
+				
 				
 			} else  {
 			
